43d08790006720186fffb05c860194458ea1b5a8
DavidRico <davidrico.dev@gmail.com>
2024-10-19 19:09:19 +0200

    Initial commit

---
+++ src/main/java/com/davidricodev/sportproject/SportProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.davidricodev.sportproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportProjectBackendApplication.class, args);
+	}
+
+}
